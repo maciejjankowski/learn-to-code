b6914cada0a4f04595f0d65e41263dcf61a3aa63
Maciej Jankowski <2905995+maciejjankowski@users.noreply.github.com>
2019-08-29 13:57:29 +0200

    data_types

---
+++ basics/00.data_types.py
@@ -0,0 +1,28 @@
+# more info: https://learnxinyminutes.com/docs/python3/
+
+imie = "Adam"
+
+print(imie[0])
+
+powitanie = "Cześć " + imie + "!"
+
+print(powitanie)
+
+wiek = 25
+
+print(f"{imie} ma {wiek} lat i {w_kieszeni} zł w kieszeni")
+
+w_kieszeni = 3.50
+
+goscie = ["Paula", "Aga", "Kamil"]
+
+goscie[0]
+goscie[1]
+
+
+
+studentka = {
+        "imie" : "Dżesika",
+        "wiek" : 20,
+        "zainteresowania": ["imprezy", "chłopaki", "takie takie"]
+        }
