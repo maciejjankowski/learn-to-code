0d6a99aa4d37d117fe33c0a28ec6d085d29fc334
Maciej Jankowski <2905995+maciejjankowski@users.noreply.github.com>
2019-10-17 16:58:27 +0200

    Update 01.ioioio.py

--- basics/01.ioioio.py
+++ basics/01.ioioio.py
@@ -1,3 +1,6 @@
+# input is a function. It takes a value inside parenthesis and returns a value.
+# That returned value can be assignmed to a variable
+
 name = input("Pls gib name: ")
 
 print("Hi! " + name)
