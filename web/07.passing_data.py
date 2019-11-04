daf84d8a777e31bd3457e4f65af70a39d73cb327
mj <maj1337+git@gmail.com>
2019-11-04 08:32:12 +0100

    passing data

--- web/07.passing_data.py
+++ web/07.passing_data.py
@@ -3,14 +3,17 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
+def voteing_page():
+    return render_template('04.form.template.html')
+
+
+@app.route('/vote', methods=['POST'])
 def vote():
-    if request.method == 'POST':
-        result = request.form
-        candidate = result.get("candidate")
-        return render_template('05.form.passing_to_js.html', 
-                                passed_data=candidate)
-    else: 
-        return render_template('04.form.template.html')
+    result = request.form
+    candidate = result.get("candidate")
+    return render_template('05.form.passing_to_js.html',
+                           passed_data=candidate)
+
 
 app.run(debug=True)
